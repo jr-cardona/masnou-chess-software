290ab2beec068ceafbfee4c2e6ed35ff8a26e514
Juan Ricardo Cardona <jr.cardona.az@gmail.com>
2025-03-17 19:58:33 -0500

    feat: prepare migration to tailwindcss

--- vite.renderer.config.mjs
+++ vite.renderer.config.mjs
@@ -1,9 +1,10 @@
 import {defineConfig} from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'node:path';
 
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [vue(), tailwindcss()],
     build: {
         rollupOptions: {
             input: {
